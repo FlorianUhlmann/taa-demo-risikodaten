fe66e008b66d0ae810c2654bfb55f831c2d4097e
UHLMANN_Florian <florian.uhlmann@soprasteria.com>
2020-11-26 15:28:09 +0100

    Co-authored-by: SCHULTHEISS Robert <robert.schultheiss@soprasteria.com>

--- src/app/risikodaten/risikodaten.component.ts
+++ src/app/risikodaten/risikodaten.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//TODO import service
+
 
 @Component({
   selector: 'app-risikodaten',
@@ -7,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RisikodatenComponent implements OnInit {
 
-  fach_risiko = "Art des Risikos"
-  fach_flaeche= "Wohnfläche"
-  names = ["Demavand", "Pradeep", "Ashutosh"];
-  constructor() { }
+  selected;
+  
+  fach_risiko  //TODO load data via service        //"Art des Risikos"
+  
+  fach_flaeche //TODO load data via service      "Wohnfläche"
+  fach_risikoTypen //TODO load data via service    [{name: "Demavand"}, {name :"Pradeep"}, {name: "Ashutosh"}];
+  
+  
+  
+  constructor(/* TODO import service */) { }
 
   ngOnInit(): void {
+    //TODO call service here AND assign data to variables fach_risiko|fach_fleache|fach_risikoTypen
   }
-
 }
+
