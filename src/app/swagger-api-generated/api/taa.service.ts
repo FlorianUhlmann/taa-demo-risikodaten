c921d88a7a56cc3ddd81a0d055786162640dcec9
UHLMANN_Florian <florian.uhlmann@soprasteria.com>
2020-11-27 15:03:30 +0100

    ammend

--- src/app/swagger-api-generated/api/taa.service.ts
+++ src/app/swagger-api-generated/api/taa.service.ts
@@ -1,4 +1,4 @@
-swagger/**
+/**
  * Swagger TAA-Strecke-Demo
  * This is a sample server TAA-Strecke-Demo server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.
  *
